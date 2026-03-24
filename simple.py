d9b28e9b82921801dd685a61ffc331f351ed0fd9
George <gveal07@outlook.com>
2026-03-24 02:06:42 +0000

    made measureable circuit

--- simple.py
+++ simple.py
@@ -1,10 +1,22 @@
 from qiskit import QuantumCircuit
+from qiskit_aer import Aer
+from qiskit import transpile
 
-# create a circuit with one qubit and classical bit
-qc = QuantumCircuit(1,1)
-
-# applies a hadamard gate to qubit 0
+# create circuit
+qc = QuantumCircuit(1, 1)
 qc.h(0)
+qc.measure(0, 0)
 
-# store reault in classical bit 0
-qc.measure(0,0)
+# simulator backend
+sim = Aer.get_backend('aer_simulator')
+
+# compile (transpile) for backend
+compiled = transpile(qc, sim)
+
+# run circuit
+result = sim.run(compiled, shots=1000).result()
+
+# get results
+counts = result.get_counts()
+
+print(counts)
